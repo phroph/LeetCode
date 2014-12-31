291c41f9022e9a1ac2312023fa333aa1852bb169
Phroph <phroph@yahoo.com>
2014-12-31 00:06:09 -0500

    Wrote logic for Graph cloning problem. Untested. Officially working in Java 1.8

--- src/Node.java
+++ src/Node.java
@@ -1,4 +1,49 @@
+import java.util.HashSet;
+import java.util.Hashtable;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Set;
+import java.util.Vector;
 
 public class Node {
-
+  Vector<Node> neighbors;
+  
+  public Node() {
+    neighbors = new Vector<>();
+  }
+  
+  public Node clone() {
+    Queue<Node> boundary = new LinkedList<>();
+    Set<Node> visited = new HashSet<>();
+    Hashtable<Node,Node> mapping = new Hashtable<>();
+    
+    boundary.add(this);
+    
+    while(!boundary.isEmpty()) {
+      Node next = boundary.poll();
+      Node newNode = new Node();     
+      if(mapping.containsKey(next)) {
+        newNode = mapping.get(next);
+      } else {
+        mapping.put(next, newNode);
+      }
+      
+      for(Node cursor : next.neighbors) {
+        if(mapping.containsKey(cursor)) {
+          newNode.neighbors.addElement(mapping.get(cursor));
+        } else {
+          Node cursorClone = new Node();
+          mapping.put(cursor, cursorClone);
+        }
+        
+        if(!visited.contains(cursor)) {
+          boundary.add(cursor);
+        }
+      }
+      
+      visited.add(next);
+    }
+    
+    return mapping.get(this);
+  }
 }
